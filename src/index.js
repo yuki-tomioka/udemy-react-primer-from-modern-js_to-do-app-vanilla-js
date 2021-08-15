4a7dc28316b00447787b57318a9b37bad5d6a43c
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-08-16 06:28:49 +0900

    タスクの追加と削除

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を入力し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li作成
+  const li = document.createElement("li");
+  li.className = "list-item";
+
+  // div作成
+  const div = document.createElement("div");
+  div.innerText = inputText;
+
+  // button（完了）タグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button（削除）タグ作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ（li）を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(div);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
