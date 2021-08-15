467d276cc1e0565f1c15ab3d05e66ebcf862e376
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-08-16 06:56:21 +0900

    完了タスクを戻す

--- src/index.js
+++ src/index.js
@@ -5,13 +5,23 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromInmompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // li作成
   const li = document.createElement("li");
   li.className = "list-item";
 
   // div作成
   const div = document.createElement("div");
-  div.innerText = inputText;
+  div.innerText = text;
 
   // button（完了）タグ作成
   const completeButton = document.createElement("button");
@@ -36,6 +46,15 @@ const onClickAdd = () => {
     // buttonタグ作成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ（li）を完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     // liタグの子要素に各要素を設定
     addTarget.appendChild(div);
@@ -60,11 +79,6 @@ const onClickAdd = () => {
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(li);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromInmompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
